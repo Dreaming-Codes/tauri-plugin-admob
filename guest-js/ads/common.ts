0520b6bc7ec2c49d09899f8134ccc067fd509aee
mohamedeliwa <mohamed.av11@gmail.com>
2025-03-21 06:00:51 +0200

    formatted ts files

--- guest-js/ads/common.ts
+++ guest-js/ads/common.ts
@@ -1,83 +1,92 @@
-import {invoke} from '@tauri-apps/api/core'
+import { invoke } from "@tauri-apps/api/core";
 
 export async function isPrivacyOptionsRequired(): Promise<boolean> {
-    return (await invoke<{isPrivacyOptionsRequired: boolean}>('plugin:admob|isPrivacyOptionsRequired')).isPrivacyOptionsRequired
+  return (
+    await invoke<{ isPrivacyOptionsRequired: boolean }>(
+      "plugin:admob|isPrivacyOptionsRequired"
+    )
+  ).isPrivacyOptionsRequired;
 }
 
 export async function showPrivacyOptionsForm(): Promise<void> {
-    await invoke('plugin:admob|showPrivacyOptionsForm')
+  await invoke("plugin:admob|showPrivacyOptionsForm");
 }
 
 export class MobileAd<T extends MobileAdOptions = MobileAdOptions> {
-    private static allAdds: { [s: number]: MobileAd } = {};
-    private static idCounter = 0;
+  private static allAdds: { [s: number]: MobileAd } = {};
+  private static idCounter = 0;
 
-    public readonly id: number;
+  public readonly id: number;
 
-    protected readonly opts: T;
+  protected readonly opts: T;
 
-    #created = false;
-    #init: Promise<void> | null = null;
+  #created = false;
+  #init: Promise<void> | null = null;
 
-    constructor(opts: T) {
-        this.opts = opts;
+  constructor(opts: T) {
+    this.opts = opts;
 
-        this.id = MobileAd.nextId();
-        MobileAd.allAdds[this.id] = this;
+    this.id = MobileAd.nextId();
+    MobileAd.allAdds[this.id] = this;
+  }
+
+  private static nextId() {
+    return MobileAd.idCounter++;
+  }
+
+  public get adUnitId() {
+    return this.opts.adUnitId;
+  }
+
+  protected async isLoaded() {
+    await this.init();
+    return await invoke("plugin:admob|adIsLoaded", {
+      id: this.id,
+    } as unknown as Record<string, unknown>);
+  }
+
+  protected async load() {
+    await this.init();
+    await invoke("plugin:admob|adLoad", {
+      ...this.opts,
+      id: this.id,
+    } as unknown as Record<string, unknown>);
+  }
+
+  protected async show() {
+    await this.init();
+    await invoke("plugin:admob|adShow", {
+      id: this.id,
+    });
+  }
+
+  protected async hide() {
+    await this.init();
+    await invoke("plugin:admob|adHide", {
+      id: this.id,
+    });
+  }
+
+  protected async init() {
+    if (this.#created) return;
+
+    if (this.#init === null) {
+      const cls =
+        (this.constructor as unknown as { cls?: string }).cls ??
+        this.constructor.name;
+
+      await invoke("plugin:admob|adCreate", {
+        ...this.opts,
+        id: this.id,
+        cls,
+      } as unknown as Record<string, unknown>);
     }
 
-    private static nextId() {
-        return MobileAd.idCounter++;
-    }
-
-    public get adUnitId() {
-        return this.opts.adUnitId;
-    }
-
-    protected async isLoaded() {
-        await this.init();
-        return await invoke('plugin:admob|adIsLoaded', {
-            id: this.id
-        } as unknown as Record<string, unknown>)
-    }
-
-    protected async load() {
-        await this.init();
-        await invoke('plugin:admob|adLoad', {
-            ...this.opts, id: this.id
-        } as unknown as Record<string, unknown>)
-    }
-
-    protected async show() {
-        await this.init();
-        await invoke('plugin:admob|adShow', {
-            id: this.id
-        })
-    }
-
-    protected async hide() {
-        await this.init();
-        await invoke('plugin:admob|adHide', {
-            id: this.id
-        })
-    }
-
-    protected async init() {
-        if (this.#created) return;
-
-        if (this.#init === null) {
-            const cls = (this.constructor as unknown as { cls?: string }).cls ?? this.constructor.name;
-
-            await invoke('plugin:admob|adCreate', {
-                ...this.opts, id: this.id, cls
-            } as unknown as Record<string, unknown>);
-        }
-
-        await this.#init;
-        this.#created = true;
-    }
+    await this.#init;
+    this.#created = true;
+  }
 }
 
 export interface MobileAdOptions {
-    adUnitId: string;
+  adUnitId: string;
 }
