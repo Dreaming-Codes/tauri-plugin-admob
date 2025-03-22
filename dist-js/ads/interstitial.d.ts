d61200ae522be59e65d8f3d869b479fdd44a645a
mohamedeliwa <mohamed.av11@gmail.com>
2025-03-23 00:24:10 +0200

    removed dist-js from gitignore

--- dist-js/ads/interstitial.d.ts
+++
@@ -1,7 +0,0 @@
-import { MobileAd } from "./common";
-export declare class InterstitialAd extends MobileAd {
-    static cls: string;
-    isLoaded(): Promise<unknown>;
-    load(): Promise<void>;
-    show(): Promise<void>;
-}
--- dist-js/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from "./ads/common";
-export * from "./ads/banner";
-export * from "./ads/interstitial";
-export * from "./ads/rewarded";
